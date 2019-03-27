8772b580834d38f449ed9ed71ed723aefcc6d6a3
Huy Cuong <17520306@gm.uit.edu.vn>
2019-03-27 13:50:02 +0700

    almost done

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,22 @@
 var router = require("express").Router();
 var mongoose = require('mongoose');
-var uploadFile=require('../services/uploadFile');
+var uploadFile = require('../services/uploadFile');
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-  const locations= await mongoose.model('locations').find().populate({
-    path:'feedbacks',
-    select:'content'
+router.get('/', async function (req, res, next) {
+  const locations = await mongoose.model('locations').find().populate({
+    path: 'feedbacks',
+    select: 'content'
   }).exec();
-	console.log("TCL: locations", JSON.stringify(locations))
+  console.log("TCL: locations", JSON.stringify(locations))
   res.render('index', { locations });
 });
 
 
 //get location by id
-router.get('/:location_id', async function(req, res, next) {
-  const locations= await mongoose.model('locations').findById(req.params.location_id).populate({
-    path:'feedbacks',
-    select:'content'
+router.get('/:location_id', async function (req, res, next) {
+  const locations = await mongoose.model('locations').findById(req.params.location_id).populate({
+    path: 'feedbacks',
+    select: 'content'
   }).exec();
   console.log('get by id');
   console.log(locations);
@@ -26,47 +26,54 @@ router.get('/:location_id', async function(req, res, next) {
 });
 
 // get data base on city
-router.get('/:city',async (req,res)=>{
-  const locations= await mongoose.model('locations').find({city:req.params.city});
-  res.render('index',{locations});
+router.get('/:city', async (req, res) => {
+  try {
+    console.log('inside get city')
+    const locations = await mongoose.model('locations').find({ city: req.params.city });
+    console.log("TCL: locations", locations)
+    res.render('index', { locations });
+  } catch (error) {
+
+  }
+
 })
 
 //add new feedback
-router.post('/feedback/:location_id',async(req,res)=>{
+router.post('/feedback/:location_id', async (req, res) => {
   try {
-    const {content}=req.body;
-    let newFeedback=await mongoose.model('feedbacks').create({...req.body,locationId:req.params.location_id});
-    
+    const { content } = req.body;
+    let newFeedback = await mongoose.model('feedbacks').create({ ...req.body, locationId: req.params.location_id });
+
     await mongoose.model('locations').update(
-      {_id:req.params.location_id},
-      {$push:{feedbacks:newFeedback}}
+      { _id: req.params.location_id },
+      { $push: { feedbacks: newFeedback } }
     )
-    
+
     res.json({
-      mess:'ok'
+      mess: 'ok'
     })
   } catch (error) {
-    
+
   }
 })
 
 //insert location
-router.post('/location',uploadFile.StoreFile().any(),async(req,res)=>{
+router.post('/location', uploadFile.StoreFile().any(), async (req, res) => {
   try {
-    let{name,address,shortDescription,city,content}=req.body;
-    let data=await mongoose.model('locations').create({
-      name:name,
-      address:address,
-      shortDescription:shortDescription,
-      city:city,
-      content:content
+    let { name, address, shortDescription, city, content } = req.body;
+    let data = await mongoose.model('locations').create({
+      name: name,
+      address: address,
+      shortDescription: shortDescription,
+      city: city,
+      content: content
     });
     res.json({
-      status:'ok',
-      message:'insert location success'
+      status: 'ok',
+      message: 'insert location success'
     })
   } catch (error) {
-    
+
   }
 })
 module.exports = router;
